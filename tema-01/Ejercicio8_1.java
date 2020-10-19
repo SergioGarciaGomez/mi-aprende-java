ec85593c21fc093eb9b2229f63299f93b56bf7bd
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-19 21:52:54 +0200

    Update Ejercicio8_1.java

--- tema-01/Ejercicio8_1.java
+++ tema-01/Ejercicio8_1.java
@@ -3,6 +3,7 @@
 	* Muestra una pirámide invertida
 	* @author Sergio García
 	*/
+
 public class Ejercicio8_1 { // Clase principal
 	public static void main (String[] args) {
 		System.out.println("*********");
