3f8f5e9015aa3d4f58eb4454c3ed1c7ba9a6b33b
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-19 12:49:18 +0200

    Ej 8

--- tema-01/ejercicio9_1.java
+++ tema-01/ejercicio9_1.java
@@ -8,18 +8,18 @@ public class ejercicio9_1 { // Clase principal
 	public static void main (String[] args) {
 		System.out.println("******************      ****             ****");
 		System.out.println("* *************** *     *  *           *   * ");
-		System.out.println("* *              * * 		*  *         *   *   ");
+		System.out.println("* *              * *    *  *         *   *   ");
 		System.out.println("* *               * *  	*	 *		   *		*    ");
-		System.out.println("* *                * *  *	 *	   *		*      ");
+		System.out.println("* *                * *  *	 *	 *		*      ");
 		System.out.println("* *                 * * *	 *   *		*        ");
 		System.out.println("* *                 * *	*	 * *		 *         ");
-		System.out.println("* *                 * * *  *	  *  *  *      ");
+		System.out.println("* *                 * * *  *	*  *  *      ");
 		System.out.println("* *                * *  *	 *	**		*	 *     ");
 		System.out.println("* *               * *   *	 * *			 *  *		 ");
 		System.out.println("* *              * *    *	 **	 	  		*	 *	 ");
 		System.out.println("* *             * *     *	 *					 *	*	 ");
 		System.out.println("* ************** *      *	 *						*	 * ");
-		System.out.println("*****************       ****						 ****");
+		System.out.println("*****************       ****	****");
 	}
 }
 
