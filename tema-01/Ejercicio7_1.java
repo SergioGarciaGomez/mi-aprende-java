19c3c54b474bdf1bc45b2d6c7cc97ef49720eeed
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-19 21:52:41 +0200

    Update Ejercicio7_1.java

--- tema-01/Ejercicio7_1.java
+++ tema-01/Ejercicio7_1.java
@@ -3,6 +3,7 @@
 	* Muestra una pirámide hueca
 	* @author Sergio García
 	*/
+
 public class Ejercicio7_1 { // Clase principal
 	public static void main (String[] args) {
 		System.out.println("    *");
