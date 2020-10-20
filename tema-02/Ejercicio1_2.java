bb5bcbab5712605ec85b123bfa41f6192e688fc3
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-20 12:56:08 +0200

    Update Ejercicio1_2.java

--- tema-02/Ejercicio1_2.java
+++ tema-02/Ejercicio1_2.java
@@ -1,8 +1,8 @@
 /**
-	* Ejercicio 1 Tema 2
-	* Muestra la suma, resta, multiplicación y división de los números 144 y 999
-	* @author Sergio García
-	*/
+ * Ejercicio 1 Tema 2
+ * Muestra la suma, resta, multiplicación y división de los números 144 y 999
+ * @author Sergio García
+ */
 	
 public class Ejercicio1_2 { // Clase principal
 	public static void main(String[] args) {
