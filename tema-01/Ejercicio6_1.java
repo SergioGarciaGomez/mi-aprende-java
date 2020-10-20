34d436e5a8b802e98eb61fc11751c01fcf7ce381
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-20 12:55:02 +0200

    Update Ejercicio6_1.java

--- tema-01/Ejercicio6_1.java
+++ tema-01/Ejercicio6_1.java
@@ -1,8 +1,8 @@
 /**
-	* Ejercicio 6 Tema 1
-	* Muestra una pirámide
-	* @author Sergio García
-	*/
+ * Ejercicio 6 Tema 1
+ * Muestra una pirámide
+ * @author Sergio García
+ */
 
 public class Ejercicio6_1 { // Clase principal
 	public static void main (String[] args) {
