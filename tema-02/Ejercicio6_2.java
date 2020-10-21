eecc8896f537cf435cb204595a484c0015feeb5b
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-21 10:10:56 +0200

    Actualización ejercicio 6

--- tema-02/Ejercicio6_2.java
+++ tema-02/Ejercicio6_2.java
@@ -10,6 +10,7 @@ public class Ejercicio6_2 { // Clase principal
 		double IVA = 21;
 		double totalFactura = (baseImponible + (baseImponible * IVA / 100));
 		System.out.printf("La base imponible de la factura es de " + baseImponible + "€ \n");
+		System.out.println("Si aplicamos el IVA que es de un 21% de la base imponible");
 		System.out.printf("La factura en total sería de " + totalFactura + "€");
 	}
 }
