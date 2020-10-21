392e4ca71521325394be7dec85b9e1c991254716
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-21 10:44:03 +0200

    Ejercicio 7

---
+++ tema-02/Ejercicio7_2.java
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 7 Tema 2
+ * Muestra 2 variables char y String en la misma línea
+ * @author Sergio García
+ */
+	
+public class Ejercicio7_2 { // Clase principal
+	public static void main(String[] args) {
+		String miVariable = "é";
+		char miLetra = 'a';
+		System.out.println(miLetra + " " + miVariable);
+	}
+}
