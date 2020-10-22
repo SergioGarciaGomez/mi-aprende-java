0c9cc3a91e5eabb6b037da5e1342366419858aff
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:59:35 +0200

    Update and rename Ejercicio2_2.java to Ejercicio02.java

---
+++ tema-02/Ejercicio02.java
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 2 Tema 2
+ * Muestra mi nombre por pantalla
+ * @author Sergio García
+ */
+public class Ejercicio02 { // Clase principal
+  public static void main(String[] args) {
+    String miNombre = "Sergio García Gómez";
+    System.out.println(miNombre);
+  }
+}
