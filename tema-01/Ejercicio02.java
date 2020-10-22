68634544a44b83a60bcacf19a0a235140c53d482
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:30:27 +0200

    Update and rename Ejercicio2_1.java to Ejercicio02.java

--- tema-01/Ejercicio02.java
+++ tema-01/Ejercicio02.java
@@ -3,7 +3,7 @@
  * Muestra tu nombre, calle y número de teléfono por pantalla
  * @author Sergio García
  */
-public class Ejercicio2_1 { // Clase principal
+public class Ejercicio02 { // Clase principal
   public static void main(String[] args) {
     System.out.println("Sergio García");
     System.out.println("Canal de la Mancha");
