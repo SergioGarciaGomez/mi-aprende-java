a26c1f5dae097ce9b3870db613523ca0338dc79a
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:42:35 +0200

    Update and rename Ejercicio9_1.java to Ejercicio09.java

---
+++ tema-01/Ejercicio09.java
@@ -0,0 +1,24 @@
+/**
+ * Ejercicio 9 Tema 1
+ * Dibujo
+ * @author Sergio García
+ */
+public class Ejercicio09 { // Clase principal
+  public static void main (String[] args) {
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+  }
+}
+
