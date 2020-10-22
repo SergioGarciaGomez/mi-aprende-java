2d053fd1ef46095c4f327f0dbbb045413610abbc
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:28:21 +0200

    Espacios

--- tema-01/Ejercicio3_1.java
+++ tema-01/Ejercicio3_1.java
@@ -3,19 +3,18 @@
  * Muestra 10 palabras en inglés y su traducción
  * @author Sergio García
  */
-	
 public class Ejercicio3_1 { // Clase principal
-	public static void main(String[] args) {
-		System.out.printf("%-10s      %-10s\n",  "Sun",  "Sol");
-		System.out.printf("%-10s      %-10s\n",  "Mouse",  "Ratón");
-		System.out.printf("%-10s      %-10s\n",  "Screen",  "Pantalla");
-		System.out.printf("%-10s      %-10s\n",  "Windows",  "Ventana");
-		System.out.printf("%-10s      %-10s\n",  "Input",  "Entrada");
-		System.out.printf("%-10s      %-10s\n",  "Output",  "Salida");
-		System.out.printf("%-10s      %-10s\n",  "Apple",  "Manzana");
-		System.out.printf("%-10s      %-10s\n",  "Channel",  "Canal");
-		System.out.printf("%-10s      %-10s\n",  "Font",  "Fuente");
-		System.out.printf("%-10s      %-10s\n",  "Keyboard",  "Teclado");
-		System.out.printf("%-10s      %-10s\n",  "Algorithm",  "Algorritmo");
-	}
+  public static void main(String[] args) {
+    System.out.printf("%-10s      %-10s\n",  "Sun",  "Sol");
+    System.out.printf("%-10s      %-10s\n",  "Mouse",  "Ratón");
+    System.out.printf("%-10s      %-10s\n",  "Screen",  "Pantalla");
+    System.out.printf("%-10s      %-10s\n",  "Windows",  "Ventana");
+    System.out.printf("%-10s      %-10s\n",  "Input",  "Entrada");
+    System.out.printf("%-10s      %-10s\n",  "Output",  "Salida");
+    System.out.printf("%-10s      %-10s\n",  "Apple",  "Manzana");
+    System.out.printf("%-10s      %-10s\n",  "Channel",  "Canal");
+    System.out.printf("%-10s      %-10s\n",  "Font",  "Fuente");
+    System.out.printf("%-10s      %-10s\n",  "Keyboard",  "Teclado");
+    System.out.printf("%-10s      %-10s\n",  "Algorithm",  "Algorritmo");
+  }
 }	
