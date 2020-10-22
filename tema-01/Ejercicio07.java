6b2c78aebf0310092ab4738892897283fb87b3ea
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:33:31 +0200

    Update and rename Ejercicio7_1.java to Ejercicio07.java

---
+++ tema-01/Ejercicio07.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 7 Tema 1
+ * Muestra una pirámide hueca
+ * @author Sergio García
+ */
+
+public class Ejercicio07 { // Clase principal
+  public static void main (String[] args) {
+    System.out.println("    *");
+    System.out.println("   * *");
+    System.out.println("  *   *");
+    System.out.println(" *     *");
+    System.out.println("*********");
+  }
+}
