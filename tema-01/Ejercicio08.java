f89321cdd80c1ff76de717ed58901d9720ac46a5
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:40:54 +0200

    Update and rename Ejercicio8_1.java to Ejercicio08.java

---
+++ tema-01/Ejercicio08.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 8 Tema 1
+ * Muestra una pirámide invertida
+ * @author Sergio García
+ */
+
+public class Ejercicio08 { // Clase principal
+  public static void main (String[] args) {
+    System.out.println("*********");
+    System.out.println(" *******");
+    System.out.println("  *****");
+    System.out.println("   ***");
+    System.out.println("    *");
+  }
+}
