76bfd635718d5417995f4705249826ab1131818b
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 10:04:33 +0200

    Update and rename Ejercicio7_2.java to Ejercicio07.java

---
+++ tema-02/Ejercicio07.java
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio 7 Tema 2
+ * Muestra 2 variables char y String en la misma línea
+ * @author Sergio García
+ */
+public class Ejercicio07 { // Clase principal
+  public static void main(String[] args) {
+    String miVariable = "é";
+    char miLetra = 'a';
+    System.out.println(miLetra + " " + miVariable);
+  }
+}
