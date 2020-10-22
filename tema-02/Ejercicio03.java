9f7e9ba26430291e14fe77daf507003ea017c87f
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 10:00:13 +0200

    Update and rename Ejercicio3_2.java to Ejercicio03.java

---
+++ tema-02/Ejercicio03.java
@@ -0,0 +1,16 @@
+/**
+ * Ejercicio 3 Tema 2
+ * Muestra mi nombre, dirección y teléfono por pantalla
+ * @author Sergio García
+ */
+	
+public class Ejercicio03 { // Clase principal
+  public static void main(String[] args) {
+    String miNombre = "Sergio García Gómez";
+    String miDireccion = "Calle Fantasía";
+    String miTelefono = "666-666-666";
+    System.out.println(miNombre);
+    System.out.println(miDireccion);
+    System.out.println(miTelefono);
+  }
+}
