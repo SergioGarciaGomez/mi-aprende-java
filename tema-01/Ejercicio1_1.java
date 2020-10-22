8f3048eeacfcc1bcd63bf13e5838bd2b9fc5178d
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:26:53 +0200

    Espacios

--- tema-01/Ejercicio1_1.java
+++ tema-01/Ejercicio1_1.java
@@ -3,7 +3,6 @@
  * Muestra tu nombre por pantalla
  * @author Sergio García
  */
-
 public class Ejercicio1_1 { // Clase principal
   public static void main(String[] args) {
     System.out.println("Sergio García");
