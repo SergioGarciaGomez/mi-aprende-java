8001c72159188f6d206bbf4ccea309c017b7bbde
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 09:27:28 +0200

    Espacios

--- tema-01/Ejercicio2_1.java
+++ tema-01/Ejercicio2_1.java
@@ -3,11 +3,10 @@
  * Muestra tu nombre, calle y número de teléfono por pantalla
  * @author Sergio García
  */
-
 public class Ejercicio2_1 { // Clase principal
-	public static void main(String[] args) {
-		System.out.println("Sergio García");
-		System.out.println("Canal de la Mancha");
-		System.out.println("666666666");
-	}
+  public static void main(String[] args) {
+    System.out.println("Sergio García");
+    System.out.println("Canal de la Mancha");
+    System.out.println("666666666");
+  }
 }
