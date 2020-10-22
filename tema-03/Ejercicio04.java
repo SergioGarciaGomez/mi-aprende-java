84a2cc669782d44209fa69dae2097b4dc020b1f9
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-22 11:29:19 +0200

    Algo

---
+++ tema-03/Ejercicio04.java
@@ -0,0 +1,10 @@
+/**
+ * Ejercicio 4 Tema 3
+ * Suma, resta, multiplica y divide 2 números introducidos
+ * @author Sergio García
+ */
+public class Ejercicio04 { // Clase principal
+  public static void main(String[] args) {
+    
+  }
+}
