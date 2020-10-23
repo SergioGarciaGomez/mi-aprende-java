cad0b9eb8bd6a178c8f743add457e4778cc06ba7
Sergio García Gómez <sergicodcraft@gmail.com>
2020-10-23 21:39:56 +0200

    Ejercicio 05 Tema 1

--- tema-01/Ejercicio05.java
+++ tema-01/Ejercicio05.java
@@ -1,1 +1,24 @@
-sdifsdfjns
+/**
+ * Ejercicio 5 Tema 1
+ * Muestra el horario de clase
+ * @author Sergio García
+ */
+public class Ejercicio05 { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("----------------------------------------------");
+    System.out.println("|\033[90m Lunes \033[39;49m|\033[90m Martes \033[39;49m|\033[90m Miérc. \033[39;49m|\033[90m Jueves \033[39;49m|\033[90m Viernes \033[39;49m|");
+    System.out.println("----------------------------------------------");
+    System.out.println("|\033[34m  EED \033[39;49m |\033[32m  PRO \033[39;49m  |\033[32m  PRO \033[39;49m  |\033[32m  PRO \033[39;49m  |\033[96m   FOL \033[39;49m  |");
+    System.out.println("----------------------------------------------");
+    System.out.println("|\033[31m  LM \033[39;49m  |\033[32m  PRO \033[39;49m  |\033[32m  PRO \033[39;49m  |\033[31m   LM \033[39;49m  |\033[96m   FOL \033[39;49m  |");
+    System.out.println("----------------------------------------------");
+    System.out.println("|\033[31m  LM \033[39;49m  |\033[34m  EED \033[39;49m  |\033[32m  PRO \033[39;49m  |\033[31m   LM \033[39;49m  |\033[96m   FOL \033[39;49m  |");
+    System.out.println("----------------------------------------------");
+    System.out.println("|\033[32m  PRO \033[39;49m |\033[34m  EED \033[39;49m  |\033[33m  BBDD \033[39;49m |\033[35m  SINF \033[39;49m |\033[35m   SINF \033[39;49m |");
+    System.out.println("----------------------------------------------");
+    System.out.println("|\033[32m  PRO \033[39;49m |\033[33m  BBDD \033[39;49m |\033[33m  BBDD \033[39;49m |\033[35m  SINF \033[39;49m |\033[35m   SINF \033[39;49m |");
+    System.out.println("----------------------------------------------");
+    System.out.println("|\033[33m  BBDD \033[39;49m|\033[33m  BBDD \033[39;49m |\033[33m  BBDD \033[39;49m |\033[35m  SINF \033[39;49m |\033[35m   SINF \033[39;49m |");
+    System.out.println("----------------------------------------------");
+  }
+}
