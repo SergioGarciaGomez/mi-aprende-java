5a42227d115d9f108d5593ee52aba6c9dbba71b4
Sergio García Gómez <sergicodcraft@gmail.com>
2020-11-03 10:00:53 +0100

    .

---
+++ tema-04/Ejercicio13.java
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 13 Tema 4
+ * Ordena 3 números de menos a mayor
+ * @author Sergio García
+ */
+public class Ejercicio13 { // Clase principal
+  public static void main(String[] args) {
+    System.out.print("Ordena 3 números de menor a mayor.\nIntroduzca 3 números: ");
+    int a = Integer.parseInt(System.console().readLine());
+    int b = Integer.parseInt(System.console().readLine());
+    int c = Integer.parseInt(System.console().readLine());
+  }
+}
