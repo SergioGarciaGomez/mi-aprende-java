b268c0295132c00841693daf27672f73d966b54a
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-11-06 18:04:06 +0100

    Ejercicio 13 Tema 4

--- tema-04/Ejercicio13.java
+++ tema-04/Ejercicio13.java
@@ -5,9 +5,30 @@
  */
 public class Ejercicio13 { // Clase principal
   public static void main(String[] args) {
+    int numero;
     System.out.print("Ordena 3 números de menor a mayor.\nIntroduzca 3 números: ");
     int a = Integer.parseInt(System.console().readLine());
     int b = Integer.parseInt(System.console().readLine());
     int c = Integer.parseInt(System.console().readLine());
+    
+    if (a > b) {
+      numero = a;
+      a = b;
+      b = numero;
+    }
+    
+    if (b > c) {
+      numero = b;
+      b = c;
+      c = numero;
+    }
+    
+    if (a > b) {
+      numero = a;
+      a = b;
+      b = numero;
+    }
+    
+    System.out.println("Los números introducidos ordenados de menor a mayor son " + a + ", " + b + " y " + c + ".");
   }
 }
