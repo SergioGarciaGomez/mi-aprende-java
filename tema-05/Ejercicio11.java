8367bef5cd33ee8e829d1d512496ccc7e11e9ac7
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-11-09 19:10:27 +0100

    Actualización Ejercicio 11 Tema 5

--- tema-05/Ejercicio11.java
+++ tema-05/Ejercicio11.java
@@ -8,9 +8,9 @@ public class Ejercicio11 { // Clase principal
   public static void main(String[] args) {
     System.out.print("Introduzca un número: ");
     int nIntroducido = Integer.parseInt(System.console().readLine());
-    System.out.print("Número Cuadrado  Cubo\n");
+    
     for (int i = nIntroducido; i < nIntroducido + 5; i++) {
-      System.out.printf("%d %7d %10d\n", i, i * i, i * i * i); 
+      System.out.printf("| Número -> %6d | Cuadrado -> %8d | Cubo -> %1d|\n", i, i * i, i * i * i); 
     }
   }
 }
