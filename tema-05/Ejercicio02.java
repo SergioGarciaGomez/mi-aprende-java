884da0590bcf55e7774cf5e32b355d1b45002432
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-11-09 16:11:17 +0100

    Ejercicio 2 y 3 Tema 5

---
+++ tema-05/Ejercicio02.java
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 2 Tema 5
+ * Muestra múltiplos de 5 desde 0-100 con bucle while
+ * @author Sergio García
+ */
+public class Ejercicio02 { // Clase principal
+  public static void main(String[] args) {
+    int i = 0;
+    while (i <= 100) {
+      System.out.println(i);
+      i+=5;
+    }
+  }
+}
---
+++ tema-05/Ejercicio03.java
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 3 Tema 5
+ * Muestra múltiplos de 5 desde 0-100 con bucle do-while
+ * @author Sergio García
+ */
+public class Ejercicio03 { // Clase principal
+  public static void main(String[] args) {
+    int i = 0;
+    do {
+      System.out.println(i);
+      i+=5;
+    } while (i <= 100)  ;
+  }
+}
