961679ecdd76938c89a0836700836fed49672a5a
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-11-22 17:58:19 +0100

    Ejercicio 16 Tema 5

--- tema-05/Ejercicio16.java
+++ tema-05/Ejercicio16.java
@@ -1,10 +1,24 @@
 /**
  * Ejercicio 16 Tema 5
- * 
+ * Programa que dice si un número introducido es o no es primo.
  * @author Sergio García
  */
 public class Ejercicio16 { // Clase principal
   public static void main(String[] args) {
+    System.out.print("Introduce un número cualquiera (positivo y entero): ");
+    int numero = Integer.parseInt(System.console().readLine());
+    boolean esPrimo = true;
     
+    for (int i = 2; i < numero; i++) {
+      if ((numero % i) == 0) {
+        esPrimo = false;
+      }
+    }
+    
+    if (esPrimo) {
+      System.out.print("El número introducido es primo.");
+    } else {
+      System.out.print("El número introducido no es primo.");
+    }
   }
 }
