91059d048836cdac65802efa42d3301acdcaa23a
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-11-24 18:45:57 +0100

    Ejercicio 40 Tema 5

--- tema-05/Ejercicio40.java
+++ tema-05/Ejercicio40.java
@@ -8,60 +8,67 @@
  */
 public class Ejercicio40 { // Clase principal
   public static void main(String[] args) {
-    
-    int alturaIntroducida = 0;
+    System.out.print("Por favor, introduzca la altura del rombo: ");
+    int alturaIntroducida = Integer.parseInt(System.console().readLine());
     int espaciado = 0;
     int espaciosDelante = alturaIntroducida / 2;
-    int altura = 1;
+    if ((alturaIntroducida < 3) || (alturaIntroducida % 2 == 0)) {
+    System.out.print("Error. Debe introducir una altura impar mayor o igual a 3");
+    } else {
+      int altura = 1;
     
-    while ((alturaIntroducida < 3) || (alturaIntroducida % 2 == 0)) {
-      System.out.print("Introduzca la altura del rombo: ");
-      alturaIntroducida = Integer.parseInt(System.console().readLine());
-      if ((alturaIntroducida < 3) || (alturaIntroducida % 2 == 0)) {
-        System.out.println("Error. Debe introducir un número impar igual o mayor a 3.");
-      }
-    }
-    
-    // Parte superior 
-    while (altura <= alturaIntroducida / 2 + 1) {
-      // Inserta los espacios delante dependiendo de la altura
-      for (int i = 1; i <= espaciosDelante; i++) {
-        System.out.print(" ");
-      }
-      // Dibuja la línea
-      for (int i = 1; i < espaciado; i++) {
-        System.out.print(" ");
-      }
+      // parte de arriba //
+      while (altura <= alturaIntroducida / 2 + 1) {
+        
+        // inserta espacios delante
+        for (int i = 1; i <= espaciosDelante; i++) {
+          System.out.print(" ");
+        }
       
-      if (altura > 1) {
+        // pinta la línea
         System.out.print("*");
-      }
-    } // While parte superior
-    
-    // Parte inferior
-    espaciado = alturaIntroducida - 3;
-    espaciosDelante = 1;
-    altura = 0;
-    
-    while (altura < alturaIntroducida / 2) {
-      // inserta los espacios delante 
-      for (int i = 1; i <= espaciosDelante; i++) {
-        System.out.print(" ");
-      }
-      // Dibuja la línea
-      System.out.print("*");
-      for (int i = 1; i < espaciado; i++) {
-        System.out.print(" ");
-      }
+        
+        for (int i = 1; i < espaciado; i++) {
+          System.out.print(" ");
+        }
+        
+        if (altura>1) {
+          System.out.print("*");
+        }
+        
+        System.out.println();
+        altura++;
+        espaciosDelante--;
+        espaciado+=2;
+        
+      } // while parte de arriba
       
-      if (altura < alturaIntroducida / 2 - 1) {
+      // parte de abajo //
+      espaciado = alturaIntroducida - 3;
+      espaciosDelante = 1;
+      altura = 0;
+      while (altura < alturaIntroducida / 2) {
+        
+        // inserta espacios delante
+        for (int i = 1; i <= espaciosDelante; i++) {
+          System.out.print(" ");
+        }
+      
+        // pinta la línea
         System.out.print("*");
-      }
+        for (int i = 1; i < espaciado; i++) {
+          System.out.print(" ");
+        }
+        
+        if(altura < alturaIntroducida / 2 - 1) {
+          System.out.print("*");
+        }
       
-      System.out.println();
-      altura++;
-      espaciosDelante++;
-      espaciado -= 2;
-    } // While parte inferior
+        System.out.println();
+        altura++;
+        espaciosDelante++;
+        espaciado -= 2;
+      } // while parte de abajo
+    } // else
   }
 }
