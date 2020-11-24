6cb4c6f447409666a5ab319eae59eec18a37d322
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-11-24 18:34:05 +0100

    Ejercicio 35 Tema 5

--- tema-05/Ejercicio40.java
+++ tema-05/Ejercicio40.java
@@ -22,6 +22,46 @@ public class Ejercicio40 { // Clase principal
       }
     }
     
-    while 
+    // Parte superior 
+    while (altura <= alturaIntroducida / 2 + 1) {
+      // Inserta los espacios delante dependiendo de la altura
+      for (int i = 1; i <= espaciosDelante; i++) {
+        System.out.print(" ");
+      }
+      // Dibuja la línea
+      for (int i = 1; i < espaciado; i++) {
+        System.out.print(" ");
+      }
+      
+      if (altura > 1) {
+        System.out.print("*");
+      }
+    } // While parte superior
+    
+    // Parte inferior
+    espaciado = alturaIntroducida - 3;
+    espaciosDelante = 1;
+    altura = 0;
+    
+    while (altura < alturaIntroducida / 2) {
+      // inserta los espacios delante 
+      for (int i = 1; i <= espaciosDelante; i++) {
+        System.out.print(" ");
+      }
+      // Dibuja la línea
+      System.out.print("*");
+      for (int i = 1; i < espaciado; i++) {
+        System.out.print(" ");
+      }
+      
+      if (altura < alturaIntroducida / 2 - 1) {
+        System.out.print("*");
+      }
+      
+      System.out.println();
+      altura++;
+      espaciosDelante++;
+      espaciado -= 2;
+    } // While parte inferior
   }
 }
