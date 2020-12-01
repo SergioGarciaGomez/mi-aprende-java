30966f8c57156e54a5458f88febdfae81ae5c232
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-12-01 11:46:42 +0100

    Ejercicio 12 Tema 6

---
+++ tema-06/Ejercicio12.java
@@ -0,0 +1,18 @@
+/**
+ * Ejercicio 12 Tema 6
+ * Realiza un programa que llene la pantalla de caracteres aleatorios (a lo Matrix) con el
+ * código ascii entre el 32 y el 126. Puedes hacer casting con (char) para convertir un
+ * entero en un carácter.
+ * @author Sergio García
+ */
+public class Ejercicio12 { // Clase principal
+  public static void main(String[] args) {
+      
+    int linea = 0;
+    System.out.print("\033[32m"); // pinta en verde a lo matrix
+    
+    for(int i = 0; i < 100000; i++) {
+      System.out.print((char)(Math.random() * (126 - 32 + 1) + 32));
+    }
+  }
+}
