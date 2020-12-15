a43029a29cf5decd68c4adb68db09878cbdef24f
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-12-15 22:32:53 +0100

    .

---
+++ tema-06/Ejercicio20.java
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 20 Tema 6
+ * 
+ * 
+ * @author Sergio García
+ */
+public class Ejercicio20 { // Clase principal
+  public static void main(String[] args) {
+    
+    
+    
+  }
+}
