dfe3006a4040c2e475ae63fa546c7ca86844a430
SergioGarciaGomez <sergicodcraft@gmail.com>
2020-12-16 07:05:39 +0100

    Ejercicio 32 Tema 6

---
+++ tema-06/Ejercicio27.java
@@ -0,0 +1,14 @@
+/**
+ * Ejercicio 27 Tema 6
+ * Implementa el juego piedra, papel y tijera. Primero, el usuario introduce su
+ * jugada y luego el ordenador genera al azar una de las opciones. Si el usuario
+ * introduce una opción incorrecta, el programa deberá mostrar un mensaje de
+ * error.
+ * @author Sergio García
+ */
+public class Ejercicio26 { // Clase principal
+  public static void main(String[] args) {
+    
+    
+  }
+}
---
+++ tema-06/Ejercicio30.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 30 Tema 6
+ * El pequeño Roberto tenía como mascota un pececillo dentro de una pecera. Los
+ * Reyes Magos le han traído un caballito de mar ($) y una caracola (@) para que
+ * le hagan compañía al pez. Realiza un programa que pinte por pantalla la pecera
+ * con los tres animalitos acuáticos colocados dentro en posiciones aleatorias. Por
+ * una cuestión de física elemental, ninguno de los animales puede coincidir
+ * en la misma posición. Se debe pedir al usuario el ancho y el alto de la pecera,
+ * que como mínimo serán de 4 unidades.
+ * @author Sergio García
+ */
+public class Ejercicio30 { // Clase principal
+  public static void main(String[] args) {
+    
+    
+  }
+}
