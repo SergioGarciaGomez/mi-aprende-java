9398ac1acd60444d9ea3f5a57fd1d66640979de8
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-02-07 12:51:05 +0100

    Ejercicio 9 Tema 1

---
+++ tema-01/Ejercicio09.java
@@ -0,0 +1,17 @@
+
+public class Ejercicio09 {
+	public static void main(String[] args) {
+		System.out.println("@@@@@@@@@@@@@**@@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@*  *@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@*    *@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@*      *@@@@@@@@@@");
+		System.out.println("@*********        *********@");
+		System.out.println("@@**                    **@@");
+		System.out.println("@@@@@**              **@@@@@");
+		System.out.println("@@@@@@@@**  ****  **@@@@@@@@");
+		System.out.println("@@@@@@@*   *@@@@*   *@@@@@@@");
+		System.out.println("@@@@@@*  *@@@@@@@@*  *@@@@@@");
+		System.out.println("@@@@@* *@@@@@@@@@@@@* *@@@@@");
+		System.out.println("@@@@*@@@@@@@@@@@@@@@@@@*@@@@");
+	}
+}
