5f1b30e2a8f3a4d35f2dbc4e71ac63a1477ba29b
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-02-08 20:17:28 +0100

    Ejercicio 17 Tema 8

--- tema-08/Ejercicio17.java
+++ tema-08/Ejercicio17.java
@@ -12,19 +12,10 @@ import java.util.Scanner;
 public class Ejercicio17 {
 
   public static void main (String[] args) {
-    long decimal = 0;
     Scanner s = new Scanner(System.in);
-    
-    System.out.println("Introduzca un número binario. ");
-    System.out.print("Empiece por 1 preferiblemente: ");
+    System.out.print("Introduzca un número binario para pasarlo a decimal: ");
     long binario = Long.parseLong(s.nextLine());
     
-    int bits = funciones.Funciones.digitos(binario);
-    
-    for(int i = 0; i < bits; i++) {
-      decimal += funciones.Funciones.digitoNumero(binario, bits - i - 1) * funciones.Funciones.potencia(2, i);
-    }
-    
-    System.out.println(binario + " en binario es " + decimal + " en decimal.");
+    System.out.println(binario + " en binario es " + funciones.Funciones.binarioADecimal(binario) + " en decimal."); 
   }
 }
