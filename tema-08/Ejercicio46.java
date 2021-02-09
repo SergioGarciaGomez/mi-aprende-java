99328f671db9a787110c84fa2ddf8ff170fce2a0
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-02-09 22:01:38 +0100

    Ejercicio 46 Tema 8

---
+++ tema-08/Ejercicio46.java
@@ -0,0 +1,34 @@
+package tema08;
+
+import java.util.Scanner;
+
+/**
+ * Ejercicio 46 Tema 8
+ * 
+ * Realiza un programa que pinte un valle en v tal como se muestra en los ejemplos. 
+ * El usuario debe introducir la altura de la figura. Podemos suponer que el 
+ * usuario introduce una altura mayor o igual a 3.
+ * 
+ * @author Sergio García
+ */
+public class Ejercicio46 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Introduzca la altura del valle en v: ");
+    int altura = Integer.parseInt(s.nextLine());
+    
+    // Pinta todo menos la última línea
+    for (int i = 1; i < altura; i++) {
+      
+      System.out.print(funciones.Funciones.vertices('*', i));
+      System.out.print(funciones.Funciones.linea(' ', altura * 2 - i * 2 - 1));
+      System.out.println(funciones.Funciones.vertices('*', i));  
+    }
+    
+    // Pinta la línea de abajo
+    System.out.print(funciones.Funciones.linea('*', altura * 2 - 1));
+    
+  }   
+}
