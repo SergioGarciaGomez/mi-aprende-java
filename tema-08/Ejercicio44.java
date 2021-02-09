eebe1da822f534687b6f8defd69ef5e8bd6c0ea4
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-02-09 19:05:48 +0100

    Ejercicio 44 Tema 8

---
+++ tema-08/Ejercicio44.java
@@ -0,0 +1,34 @@
+package tema08;
+
+import java.util.Scanner;
+
+/**
+ * Ejercicio 44 Tema 8
+ * 
+ * Realiza un programa que pinte un triángulo hueco tal como se muestra en los
+ * ejemplos. El usuario debe introducir la altura de la figura. Utiliza
+ * funciones para pintar las líneas.
+ * 
+ * @author Sergio García
+ */
+public class Ejercicio44 {
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    System.out.print("Introduzca la altura de la figura: ");
+    int altura = Integer.parseInt(s.nextLine());
+    
+    System.out.println(funciones.Funciones.linea('*',  altura));
+    
+    for (int i = 1; i < altura; i++) {
+      System.out.print(funciones.Funciones.linea(' ', i));
+      System.out.print("*" + funciones.Funciones.linea(' ', altura - i - 2));
+      
+      if (i < altura - 1) {
+        System.out.println("*");
+      }
+    }
+    System.out.println();
+  }
+}
